ccb84c9b41b01a71dbf1057e35bd5893b20740b5
antoniokallo <antoniokallo@gmail.com>
2022-07-20 23:56:24 -0400

    commit tree d2dd254d25eb5ef8598d206ff34ca5a15f3975ca

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById('main')
+console.log(main)
+const newHeader = document.createElement('h1')
+
+main.remove();
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Antonio is the champion'
+document.body.append(newHeader)
